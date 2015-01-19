0fcf3e55f73ca6dd46e38b1d43d5dae513ee129d
jeasonyoung <jeason1914@qq.com>
2015-01-19 21:00:07 +0800

    初始化

---
+++ ExamApp/ExamApp/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  ExamApp
+//
+//  Created by jeasonyoung on 15/1/19.
+//  Copyright (c) 2015年 com.examw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
