fe24c49b9282f513feea22d4eaef18ecab1ba377
jeasonyoung <jeason1914@qq.com>
2015-05-13 11:19:00 +0800

    重构项目

---
+++ ExamwTestApp/ExamwTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExamwTestApp
+//
+//  Created by jeasonyoung on 15/5/13.
+//  Copyright (c) 2015年 com.examw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
